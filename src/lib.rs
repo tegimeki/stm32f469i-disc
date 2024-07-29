427d621c1b27ef906046c67f1145a492717e2fc1
Michael Fairman <michael.fairman@chargepoint.com>
2024-07-29 06:00:58 -0700

    stm32f469i-disc: initial BSP
    Supports LEDs to start with.  Uses an `ErasedPin` type because unlike
    the other boards the LEDs are spread across a few GPIO peripherals.

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![no_std]
+#![allow(non_camel_case_types)]
+
+pub use stm32f4xx_hal as hal;
+
+pub use crate::hal::pac::interrupt::*;
+pub use crate::hal::pac::Interrupt;
+pub use crate::hal::pac::Peripherals;
+
+pub mod led;
